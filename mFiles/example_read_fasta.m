2b2cc5263f61035f41f7e0767fd38d32b656ddb2
Or Zuk <oorzuk@gmail.com>
2013-10-06 18:41:00 +0300

    Added debug variables

--- mFiles/example_read_fasta.m
+++ mFiles/example_read_fasta.m
@@ -1,6 +1,8 @@
 % an example of loading reads from a fasta file and converting them to a COMPASS format
 
-userDir = '~/CS/mFilesBAC/COMPASS/'; % ENTER THE PATH TO THE COMPASS DIRECTORY
+if(~exist('userDir', 'var') || isempty(userDir))
+  userDir = '~/CS/mFilesBAC/COMPASS/'; % ENTER THE PATH TO THE COMPASS DIRECTORY
+end
 
 % load the fasta file
 % the file should include reads of constant length - Hence first filter the reads and trim them to constant length
@@ -11,7 +13,7 @@ seq_matrixForm = cell2mat(seq');
 readLength = 90; % the read length
 
 % COMPASS reads 
-[uniqueReads,uniqueReads_length]=createUniqueReadsAndPack(seq_matrixForm,readLength);
+[uniqueReads, uniqueReads_length, packed_seqs] = createUniqueReadsAndPack(seq_matrixForm,readLength);
 
 % uniqueReads is a list of unique reads. Each read is saved as a 64bit word.
 % uniqueReads_length is the number of times each such read appears
